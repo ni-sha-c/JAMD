697967054e55139599e728f02cdb661376c275a3
Nisha Chandramoorthy <nishac92@gmail.com>
2015-12-06 23:11:05 -0500

    Add SharedArray parallelism

--- src/JAMD_prelim_parallel.jl
+++ src/JAMD_prelim_parallel.jl
@@ -1,19 +1,18 @@
-module mdcall
-using PyPlot
-include("kin_init.jl")
-include("MD_calc.jl")
-const plt = PyPlot
-plt.pygui(true)
-using MD_calc
+#module mdcall
+#using PyPlot
+@everywhere using kin_init
+@everywhere using MD_calc
+#const plt = PyPlot
+#plt.pygui(true)
 #plt.close()
 #reload("MD_calc")
 
-Ni = 8#2#8; #number of atoms per side in original cubic configuration 
+Ni = 2#2#8; #number of atoms per side in original cubic configuration 
 Ts = 1.0; #desired (and initial) temperature in LJ units (Ts = kB*T/epsilon)
 ns = 0.05; #LJ number density ns = n*sigma^3
 
 rc = 3.0; #cut-off radius, in LJ units
-STEPS = 5000#1e4; #number of timesteps
+STEPS = 500#1e4; #number of timesteps
 STEPS_thermostat = round(Int64,(STEPS/10)) #3*10^3; #number of steps to leave thermostat on until
 STEPS_equilib = round(Int64,(STEPS/2)) #4*10^3; #number of steps before starting to average
 alpha = 0.1; #thermostat relaxation parameter 
@@ -31,6 +30,7 @@ r = zeros(3,N);
 v = zeros(3,N);  
 F = zeros(3,N); 
 U = zeros(1,N); 
+
 Res = zeros(STEPS,2); 
 #positions = zeros(N,3,STEPS);
 
@@ -40,21 +40,26 @@ v = broadcast(-,v,c');
 T_inst =2/(3.0*N)*0.5*sum(v.^2)
 t=1
 println(T_inst)   
+#addprocs(4)
+F = convert(SharedArray,zeros(3,N))
+	U = convert(SharedArray,zeros(1,N))
+ 	r = convert(SharedArray,r)
+	v = convert(SharedArray,v)
+
 @time for t=1:STEPS
 	
-	F = zeros(3,N)
-	U = zeros(1,N)
-  
-    MD_calc.force_calculation(N,r,Ls,rc2,F,U);
-  	v,r,T_inst = MD_calc.take_a_step(Ts,dt,N,STEPS_thermostat,alpha,t,F,v,r,Ls)
+		
+    MD_calc.force_calculation_parallel(N,r,Ls,rc2,F,U);
+  	v,r,T_inst = MD_calc.take_a_step_parallel(Ts,dt,N,STEPS_thermostat,alpha,t,F,v,r,Ls)
 	T_inst =2/(3.0*N)*0.5*sum(v.^2)
-	
+	#println(T_inst)
 	if(T_inst > 10.0)
 			break
 	end
+	
 	#plt.scatter3D(r[1,:],r[2,:],r[3,:])
 	#plt.hold(true)
 	
 end
 
-end
+#end
--- src/MD_calc.jl
+++ src/MD_calc.jl
@@ -1,7 +1,7 @@
-module MD_calc
+@everywhere module MD_calc
 
-include("particle_manipulations.jl")
-include("potential.jl")
+#include("particle_manipulations.jl")
+#include("potential.jl")
 #################################
 #Initialization
 function set_MD_params(n::Int64=27,
@@ -62,6 +62,53 @@ end
 POS = POS + DT*V
 [POS[i] = POS[i] >=LS ? POS[i]-LS: POS[i] for i = 1:length(POS)]
 [POS[i] = POS[i] <0 ? POS[i]+LS: POS[i] for i = 1:length(POS)]
+
+
+return V,POS,T_inst
+end
+
+############################
+#Parallel take_a_step
+
+#Velocity Verlet
+function take_a_step_parallel(TS::Float64,
+		DT::Float64,
+		N::Int64,
+		STEPS_THERMOSTAT::Int64,
+		α::Float64,
+		t::Int64,
+		F::SharedArray{Float64,2},
+		V::SharedArray{Float64,2},
+		POS::SharedArray{Float64,2},LS::Float64)
+
+
+
+VCM = mean(V,2);
+V_THERM_SQ =0.e0
+#for i = 1:3*size(POS,1)
+	  V = V +DT/2.0*F
+	  V = broadcast(-,V,VCM)
+	  #V[i] = V[i] + DT/2.0*F[i] - VCM[ceil(Int64,i/N)]
+	 
+for i = 1:3*size(POS,2)	  
+	 
+	  if( t < STEPS_THERMOSTAT)	
+		V_THERM_SQ += (V[i])^2.0
+	  end
+end
+
+T_inst = 2/(3*N)*0.5*V_THERM_SQ;
+c = sqrt( 1+α*(TS/T_inst-1) )
+if( t < STEPS_THERMOSTAT)	
+V = V*c
+end
+
+
+@sync @parallel for i = 1:length(POS)
+		POS[i] = POS[i] + DT*V[i]
+		POS[i] = POS[i] >=LS ? POS[i]-LS: POS[i] 
+		POS[i] = POS[i] <0 ? POS[i]+LS: POS[i] 
+end
 
 
 return V,POS,T_inst
@@ -130,6 +177,66 @@ function force_calculation(N::Int64,
 
 
 
+end
+
+#### Parallel Force Calculation
+
+function force_calculation_parallel(N::Int64,
+pos::SharedArray{Float64,2},
+Ls::Float64,
+rc2::Float64,
+F::SharedArray{Float64,2},
+U::SharedArray{Float64,2})
+
+
+
+@sync @parallel for i = 1:3:3*(N-1)
+@sync @parallel for j = i+3:3:3*N
+xij = pos[i]-pos[j]
+yij = pos[i+1]-pos[j+1]
+zij = pos[i+2] - pos[j+2]
+if(xij > Ls/2.0)
+xij-=Ls
+end
+if(xij < -Ls/2.0)
+xij+=Ls
+end
+if(yij > Ls/2.0)
+yij-=Ls
+end
+if(yij < -Ls/2.0)
+yij+=Ls
+end
+if(zij > Ls/2.0)
+zij-=Ls
+end
+if(zij < -Ls/2.0)
+zij+=Ls
+end
+
+
+rij2 = xij^2.0 + yij^2.0 + zij^2.0
+rij = sqrt(rij2)
+if(rij2 < rc2^2)
+p_i= ceil(Int64,i/3)
+p_j = ceil(Int64,j/3)
+rij6 = 1./(rij2^3.0)
+rij12 = 1./(rij2^6.0)
+c =4.e0*(rij12-rij6)
+d =24.e0/rij2*(2.0*rij12-rij6)
+U[p_i] += c
+U[p_j] += c
+
+F[i] += d*xij
+F[i+1] += d*yij
+F[i+2] += d*zij
+F[j] += -d*xij
+F[j+1] += -d*yij
+F[j+2] += -d*zij
+end
+
+end
+end
 end
 
 end
